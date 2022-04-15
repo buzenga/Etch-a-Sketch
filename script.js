47827b3c73a3be062b1713be2535796762711b37
Kuba <gliwkajakub@gmail.com>
2022-04-16 00:54:53 +0200

    add touch function

--- script.js
+++ script.js
@@ -3,16 +3,63 @@ const submitGridButton = document.querySelector(".submit-grid");
 const slider = document.getElementById("myRange");
 const divContainer = document.querySelector(".container");
 const sketchDivs = document.querySelectorAll('.sketch-div');
+const blackButton = document.querySelector('.black');
+const grayButton = document.querySelector('.gray');
+const rainbowButton = document.querySelector('.rainbow');
+const clearButton = document.querySelector('.clear');
+
+const clicked = document.querySelector(".clicked");
 
 let numberOfDivs = 16;
+let mouseClickedDown = false;
+
 
 slider.addEventListener("change", (e) => {
    gridDisplay.innerText = slider.value;
    numberOfDivs = slider.value;
 })
 
+document.addEventListener('mousemove', (e) => {
+   clicked.innerText = mouseClickedDown;
+})
+//-----------------------------------------------
+window.addEventListener("mouseup", (e) => {
+   mouseClickedDown = false;
+   return;
+})
+//----------------------------------------------
+
 submitGridButton.addEventListener('click', (e) => {
-   console.log(`divów będzie ${numberOfDivs}`)
+
+   createDivs(numberOfDivs);
+   blackpaint();
+   blackpaintTouch();
+
+   // const sketchDivs = document.querySelectorAll('.sketch-div');
+
+   // mouseClickedDown = false;
+   // sketchDivs.forEach(div => div.addEventListener("mousedown", (e) => {
+
+   //    mouseClickedDown = true;
+   //    console.log("wcisniete");
+
+   //    sketchDivs.forEach(div => div.addEventListener("mousemove", (e) =>  {
+         
+   //       if ( mouseClickedDown == true) {
+   //       div.style.backgroundColor = "black";
+   //       } 
+   //    }))
+   //    sketchDivs.forEach(div => document.addEventListener("mouseup", (e) => {
+   //       mouseClickedDown = false;
+   //    }))
+   // }))
+
+
+   mouseClickedDown = false;
+})
+
+function createDivs (numberOfDivs) {
+
    divContainer.innerHTML = "";
 
    for (i=0; i < (numberOfDivs * numberOfDivs); i++) {
@@ -23,37 +70,88 @@ submitGridButton.addEventListener('click', (e) => {
    divContainer.style.display = "grid";
    divContainer.style.gridTemplateColumns = `repeat(${numberOfDivs}, 1fr)`;
    divContainer.style.gridTemplateRows = `repeat(${numberOfDivs}, 1fr)`;
+}
 
-
+const blackpaint = () => {
    const sketchDivs = document.querySelectorAll('.sketch-div');
-
-   let mouseClickedDown = false;
+   
+   mouseClickedDown = false;
    sketchDivs.forEach(div => div.addEventListener("mousedown", (e) => {
 
       mouseClickedDown = true;
-      console.log("wcisniete");
+      div.style.backgroundColor = "black";
+
       sketchDivs.forEach(div => div.addEventListener("mousemove", (e) =>  {
+         
+
          if ( mouseClickedDown == true) {
          div.style.backgroundColor = "black";
-         } else {
-            return;
-         }
+         } else return;
       }))
-      sketchDivs.forEach(div => div.addEventListener("mouseup", (e) => {
+      sketchDivs.forEach(div => window.addEventListener("mouseup", (e) => {
          mouseClickedDown = false;
+         return;
       }))
    }))
-
-   // sketchDivs.forEach(div => div.addEventListener('mousemove', (e) => {
-   //    console.log('cos niecos')
-   //    div.style.backgroundColor = "black";
-   // }))
-})
-
-function createDivs (numberOfDivs) {
-
 }
-// sketchDivs.forEach(div => div.addEventListener('mouseover', (e) => {
-//    console.log('cos niecos')
-//    div.style.backgroundColor = "black";
-// }))
+const blackpaintTouch = () => {
+   const sketchDivs = document.querySelectorAll('.sketch-div');
+   
+   mouseClickedDown = false;
+   sketchDivs.forEach(div => div.addEventListener("touchstart", (e) => {
+
+      mouseClickedDown = true;
+      div.style.backgroundColor = "black";
+
+      sketchDivs.forEach(div => div.addEventListener("touchmove", (e) =>  {
+         
+
+         if ( mouseClickedDown == true) {
+         div.style.backgroundColor = "black";
+         } else return;
+      }))
+      sketchDivs.forEach(div => window.addEventListener("touchend", (e) => {
+         mouseClickedDown = false;
+         return;
+      }))
+   }))
+}
+
+
+
+
+
+// const blackpaint = () => {
+// //    const sketchDivs = document.querySelectorAll('.sketch-div');
+   
+// //    mouseClickedDown = false;
+
+// //    divContainer.addEventListener("mousedown", () => {
+// //       mouseClickedDown = true;
+// //    })
+// //    window.addEventListener("mouseup", () => {
+// //       mouseClickedDown = false;
+// //    })
+// //    sketchDivs.forEach(div => div.addEventListener("mouseover", () => {
+// //       if (mouseClickedDown == true) {
+// //          div.style.backgroundColor = "pink";
+// //       }
+// //    }))
+
+//    sketchDivs.forEach(div => div.addEventListener("mousedown", (e) => {
+
+//       mouseClickedDown = true;
+
+//       sketchDivs.forEach(div => div.addEventListener("mousemove", (e) =>  {
+         
+
+//          if ( mouseClickedDown == true) {
+//          div.style.backgroundColor = "pink";
+//          } else return;
+//       }))
+//       sketchDivs.forEach(div => window.addEventListener("mouseup", (e) => {
+//          mouseClickedDown = false;
+//          return;
+//       }))
+//    }))
+// }
