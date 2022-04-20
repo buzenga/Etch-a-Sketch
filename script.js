67cf3730faee7567e3101160740268cf740a3ff4
Kuba <gliwkajakub@gmail.com>
2022-04-20 23:18:31 +0200

    Add hover animations to buttons

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ const grayButton = document.querySelector('.gray');
 const rainbowButton = document.querySelector('.rainbow');
 const clearButton = document.querySelector('.clear');
 
+const playerInfo = document.querySelector(".player-info");
+
 
 let numberOfDivs = 16;
 let mouseClickedDown = false;
 let mode = '';
 let color = '';
+let readyToSketch = false;
 
 
 slider.addEventListener("change", (e) => {
@@ -22,21 +25,40 @@ slider.addEventListener("change", (e) => {
 
 submitGridButton.addEventListener('click', (e) => {
    color = 'black';
+   mode = 'black';
+   resetButtons();
+
    createDivs(numberOfDivs);
+   readyToSketch = true;
    paint ();
+   if (readyToSketch === true) {
+      playerInfo.innerText = "You can start sketching";
+   }
 })
 
 blackButton.addEventListener("click", () => {
    mode = "black";
    color = "black";
+   resetButtons();
+   // blackButton.classList.add("black-running")
 })
 grayButton.addEventListener("click", () => {
    mode = "gray";
+
+   resetButtons();
+   // grayButton.classList.add("gray-running")
 })
 rainbowButton.addEventListener("click", () => {
    mode = "rainbow";
+
+   resetButtons();
+   // rainbowButton.classList.add("rainbow-running")
 })
 clearButton.addEventListener("click", () => {
+   resetButtons();
+   if (readyToSketch === true) {
+      playerInfo.innerText = "You can start sketching";
+   }
    const sketchDivs = document.querySelectorAll('.sketch-div');
    sketchDivs.forEach(div => {
       div.style.backgroundColor = '';
@@ -61,6 +83,7 @@ const paint = () => {
    mouseClickedDown = false;
 
    sketchDivs.forEach(div => div.addEventListener('mousedown', (e) => {
+      playerInfo.innerText = "";
       mouseClickedDown = true;
       switch(mode) {
          case 'gray':
@@ -104,4 +127,21 @@ const paint = () => {
          }
       } else return;
    }))
+}
+const resetButtons = () => {
+   grayButton.classList.remove("gray-running");
+   rainbowButton.classList.remove("rainbow-running");
+   blackButton.classList.remove("black-running");
+
+   switch (mode) {
+      case 'black':
+         blackButton.classList.add("black-running");
+         break;
+      case 'gray':
+         grayButton.classList.add("gray-running");
+         break;
+      case 'rainbow':
+         rainbowButton.classList.add("rainbow-running");
+         break;
+   }
 }
